e467c0639bbdffd1c03a630e77074c7ea34ad29e
rbyrne <rbyrne>
2005-11-07 23:16:15 +0000

    remove an extraneous initialializer from the stream2 type struct

--- src/log4c/appender_type_stream2.c
+++ src/log4c/appender_type_stream2.c
@@ -204,7 +204,6 @@ const log4c_appender_type_t log4c_appender_type_stream2 = {
     stream2_open,
     stream2_append,
     stream2_close,
-    stream2_init,
 };
 
 log4c_appender_type_define(log4c_appender_type_stream2);
